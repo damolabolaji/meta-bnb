16162573dc1c71a82bcedca1f31f8344f02d69ed
damolabolaji <bolaji.oyindamola@gmail.com>
2022-11-12 19:35:10 +0100

    feat: nav on small screen, style: media query for 520px

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,24 @@
 //logo gradient DC3E88, B840AE, 843FE8
 
 import { Link, Route, Routes } from 'react-router-dom';
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+
 import Home from '../Home';
 import PlaceToStay from '../Placestostay'
 import MetaLogo from "../images/logos/meta-logo-color.png";
 
+
+
 const NavBar = () => {
+
+    const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
     return ( 
         
          <Routes>
@@ -23,15 +36,25 @@ const NavBar = () => {
             </Link>
   
 
-            <ul>
+            <ul ref={navRef}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/Placestostay">Places to Stay</Link></li>
                 <li><a href="">NFTs</a></li>
                 <li><a href="">Community</a></li>
+                <Link to="/" className="btn connect-btn">Connect Wallet</Link>
+                <button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
             </ul>
 
-            <Link to="/" className="btn connect-btn">Connect Wallet</Link>
-            
+         
+
+            <button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+
         </nav>
      );
 }
