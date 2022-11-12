a52aaa3dbafd4289be517ccb4b8b9d05e842ddcb
damolabolaji <bolaji.oyindamola@gmail.com>
2022-11-12 22:32:13 +0100

    feat: impl. dropdown menu. main and sec navigation

--- src/Placestostay.js
+++ src/Placestostay.js
@@ -1,25 +1,65 @@
 
 import AllPlaces from "./AllPlaces";
+import { useRef } from "react";
+import SettIcon from './images/icons/setting.png'
+import PlacesCard from './components/Places';
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+
+
 
 
 const PlaceToStay = () => {
+
+    const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
     return ( 
        
         <div>
-           <div>
-            <ul>
-                <li><a href="">Restaurant</a></li>
-                <li><a href="">Cottage</a></li>
-                <li><a href="">Castle</a></li>
-                <li><a href="">fantast city</a></li>
-                <li><a href="">beach</a></li>
-                <li><a href="">Cabins</a></li>
-                <li><a href="">Off-grid</a></li>
-                <li><a href="">Farm</a></li>
-            </ul>
-           </div>
+           <nav className="seconday-nav">
+                <ul ref={navRef}>
+                    <li><a href="">Restaurant</a></li>
+                    <li><a href="">Cottage</a></li>
+                    <li><a href="">Castle</a></li>
+                    <li><a href="">fantasy city</a></li>
+                    <li><a href="">beach</a></li>
+                    <li><a href="">Cabins</a></li>
+                    <li><a href="">Off-grid</a></li>
+                    <li><a href="">Farm</a></li>
+                    <FaArrowUp onClick={showNavbar} className="nav-close-btn"/> 
+                </ul>
 
-            <AllPlaces/>
+
+
+                <button className="nav-btn" onClick={showNavbar}>
+                    Choose Selection <FaArrowDown/> 
+                
+                </button>
+
+
+                <button className="btn" >
+                    location <img src={SettIcon} alt="" /> 
+                </button>
+           </nav>
+
+           <section className="featured">
+            
+
+            <div className="featured-places">
+                <PlacesCard/>
+                <PlacesCard/>
+                <PlacesCard/>
+                <PlacesCard/>
+                <PlacesCard/>
+                <PlacesCard/>
+                <PlacesCard/>
+                <PlacesCard/>
+ 
+            </div>
+        </section>
         </div>
      );
 }
