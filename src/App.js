62bc69454cb1dc7d6d895226ca067d9c01cb6e00
damolabolaji <bolaji.oyindamola@gmail.com>
2022-11-14 00:39:24 +0100

    style: responsiveness for modal

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import NavBar from './components/Navbar'
 import Footer from './components/Footer';
 import Home from './Home';
 import PlaceToStay from './Placestostay'
-
+import Modal from './components/Modal'
 
 import { Route, Routes } from 'react-router-dom'
+import { useState } from "react";
 
 
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="App">
 
@@ -23,9 +25,9 @@ function App() {
 
 
 
-    
+    <button  onClick={() => setIsOpen(true)}>modal bitch</button>
      <Footer />
-
+     {isOpen && <Modal setIsOpen={setIsOpen} />}
     </div>
   );
 }
