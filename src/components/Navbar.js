0f039a5fced6c7a1a2050233cbdb903904e28199
damolabolaji <bolaji.oyindamola@gmail.com>
2022-11-15 13:48:50 +0100

    refactor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,8 +46,8 @@ const NavBar = () => {
             <ul ref={navRef}>
                 <li><Link to="/" onClick={showNavbar}>Home</Link></li>
                 <li><Link to="/Placestostay" onClick={showNavbar}>Places to Stay</Link></li>
-                <li><a href="" onClick={showNavbar}>NFTs</a></li>
-                <li><a href="" onClick={showNavbar}>Community</a></li>
+                <li><a href="/" onClick={showNavbar}>NFTs</a></li>
+                <li><a href="/" onClick={showNavbar}>Community</a></li>
                 <Link to="/" className="btn connect-btn" onClick={() => {setIsOpen(true)}}>Connect Wallet</Link>
                 <button
 					className="nav-btn nav-close-btn"
